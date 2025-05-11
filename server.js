4064f9926a44bf987d8e28f6cad515bd290648f8
Arthur Weale <ajayrossy340@gmail.com>
2025-05-11 04:00:30 +0200

    Fix: moved view engine to be above routes

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+
 /* ***********************
  * Routes
  *************************/
@@ -21,13 +32,9 @@ app.get("/", function(req,res){
   res.render("index", {title: "Home"})
 })
 
-/* ***********************
- * View Engine and Templates
- *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-
+// app.get("/", (req, res) => {
+//   res.send("Heyyyy");
+// });
 
 
 /* ***********************
